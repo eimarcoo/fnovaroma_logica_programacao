72e9951e71c5e41da41f4616491cb37a600baf18
eimarcoo <eimarcoo@outlook.com>
2023-03-14 20:54:30 -0300

    criação de arquivo projeto hello

---
+++ Hello.java
@@ -0,0 +1,12 @@
+
+public class Hello {
+
+    public static void main (String[] args) {
+        System.out.println("HELLO WORLD");
+        System.out.println((1+1));
+        System.out.println((2*3-(6-4)));
+        System.out.println(1-5+6/54);
+
+    }
+
+} 
