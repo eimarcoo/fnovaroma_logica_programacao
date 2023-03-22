3427562fe2b190343bdc7f9b3d2ce8d789c76e4f
eimarcoo <eimarcoo@outlook.com>
2023-03-21 21:13:52 -0300

    atualização

---
+++ Teste.java
@@ -0,0 +1,11 @@
+public class Teste
+{
+    public static void main(String [] args){
+        float faculdade = 5;
+        int teste = 10;
+        float divisao = faculdade/teste;
+        System.out.println(divisao);
+        divisao = faculdade;
+        System.out.println(divisao);
+    }
+}
